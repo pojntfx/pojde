07efd57118f915357b52d477bc404735c924fc50
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-02-21 14:45:22 +0100

    feat: Add user

---
+++ configuration/user.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Read configuration file
+source /opt/pojde-ng/preferences/preferences.sh
+
+# Set new root password
+echo "root:${POJDE_NG_ROOT_PASSWORD}" | chpasswd
+
+# Create new user and add them to the wheel group
+addgroup --system wheel
+useradd "${POJDE_NG_USERNAME}"
+sed -i 's/#auth required pam_wheel.so/auth required pam_wheel.so/g' /etc/pam.d/su
+adduser "${POJDE_NG_USERNAME}" wheel
+adduser "${POJDE_NG_USERNAME}" sudo
+
+# Change the password for the new user
+echo "${POJDE_NG_USERNAME}:${POJDE_NG_PASSWORD}" | chpasswd
+
+# Use bash as the default shell for the new user
+chsh -s /bin/bash "${POJDE_NG_USERNAME}"
