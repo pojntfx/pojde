fb6de5c29328d3b6cfb59f464eac4ab4e61ddbe9
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-02-22 01:24:18 +0100

    feat: Set pasword for ttyd & setup service

--- build/ttyd.sh
+++ build/ttyd.sh
@@ -6,13 +6,17 @@ curl -L -o /usr/bin/ttyd https://github.com/tsl0922/ttyd/releases/download/${VER
 chmod +x /usr/bin/ttyd
 
 # Create systemd service with the listen port set to 38002
-cat <<EOT >/usr/lib/systemd/system/ttyd.service
+cat <<EOT >/usr/lib/systemd/system/ttyd@.service
 [Unit]
 Description=ttyd
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/ttyd -i lo -p 38002 bash
+EnvironmentFile=/opt/pojde-ng/ttyd/flags.sh
+ExecStart=/usr/bin/ttyd -i lo -p 38002 -c \${USERNAME_PASSWORD} bash
+WorkingDirectory=/home/%i
+Restart=always
+User=%i
 
 [Install]
 WantedBy=multi-user.target
