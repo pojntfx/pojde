d0dfac925e3a1781aa69e81b8a3d4ef2caf4ac17
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-02-23 01:20:49 +0100

    feat: Make SSH key source configurable

--- configuration/parameters.sh
+++ configuration/parameters.sh
@@ -21,8 +21,11 @@ echo export "'"POJDE_NG_PASSWORD="$(dialog --stdout --nocancel --insecure --pass
 echo export "'"POJDE_NG_IP="$(dialog --stdout --nocancel --inputbox "Enter the new IP or press ENTER to keep the current one:" 0 0 ${POJDE_NG_IP})""'" >>${PREFERENCE_FILE}
 echo export "'"POJDE_NG_DOMAIN="$(dialog --stdout --nocancel --inputbox "Enter the new domain or press ENTER to keep the current one:" 0 0 ${POJDE_NG_DOMAIN})""'" >>${PREFERENCE_FILE}
 
-# Ask for GitHub username
-echo export "'"POJDE_NG_GITHUB_USERNAME="$(dialog --stdout --nocancel --inputbox "Enter your GitHub username or press ENTER to keep the current one:" 0 0 ${POJDE_NG_GITHUB_USERNAME})""'" >>${PREFERENCE_FILE}
+# Ask for SSH key URL; get from GitHub by default
+if [ -z "${POJDE_NG_SSH_KEY_URL}" ]; then
+    export POJDE_NG_SSH_KEY_URL="https://github.com/${POJDE_NG_USERNAME}.keys"
+fi
+echo export "'"POJDE_NG_SSH_KEY_URL="$(dialog --stdout --nocancel --inputbox "Enter a link to your SSH keys or press ENTER to keep the current one:" 0 0 ${POJDE_NG_SSH_KEY_URL})""'" >>${PREFERENCE_FILE}
 
 # Ask for confirmation
 dialog --yesno 'Are you sure you want apply the configuration?' 0 0 || exit 1
