56bb481a0a0d11c4b2a3696c5a04bbdff4898698
Felix Pojtinger <felix@pojtinger.com>
2021-02-23 01:20:49 +0100

    feat: Make SSH key source configurable

--- configuration/ssh.sh
+++ configuration/ssh.sh
@@ -5,5 +5,5 @@ source /opt/pojde-ng/preferences/preferences.sh
 
 # Add the user's SSH keys to authorized_keys
 mkdir -m 700 -p /root/.ssh
-curl -L "https://github.com/${POJDE_NG_GITHUB_USERNAME}.keys" | tee /root/.ssh/authorized_keys
+curl -L "${POJDE_NG_SSH_KEY_URL}" | tee /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
