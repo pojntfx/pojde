58826440544828338c68b6462eed442c3680234a
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-02-24 23:01:03 +0100

    feat: Add OpenRC service for ttyd

--- configuration/ttyd.sh
+++ configuration/ttyd.sh
@@ -3,13 +3,25 @@
 # Read configuration file
 source /opt/pojde-ng/preferences/preferences.sh
 
-# Change the password to the new value
+# Create config file
 mkdir -p /opt/pojde-ng/ttyd
-CONFIG_FILE=/opt/pojde-ng/ttyd/flags.sh
-cat <<EOT >$CONFIG_FILE
+CONFIG_FILE=/opt/pojde-ng/ttyd/env
+
+# Enable & restart the services
+if [ "${POJDE_NG_OPENRC}" = 'true' ]; then
+    # Change the password to the new value
+    cat <<EOT >$CONFIG_FILE
+'${POJDE_NG_USERNAME}:${POJDE_NG_PASSWORD}'
+EOT
+
+    rc-service ttyd restart
+    rc-update add ttyd default
+else
+    # Change the password to the new value
+    cat <<EOT >$CONFIG_FILE
 USERNAME_PASSWORD='${POJDE_NG_USERNAME}:${POJDE_NG_PASSWORD}'
 EOT
 
-# Enable & restart the service
-systemctl enable "ttyd@${POJDE_NG_USERNAME}"
-systemctl restart "ttyd@${POJDE_NG_USERNAME}"
+    systemctl enable "ttyd@${POJDE_NG_USERNAME}"
+    systemctl restart "ttyd@${POJDE_NG_USERNAME}"
+fi
