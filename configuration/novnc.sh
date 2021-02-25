8507dbcf4bcdb2d03f9e58ccbf69f360df959c1e
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-02-25 13:03:12 +0100

    refactor: Make OpenRC with Fluxbox slightly more accessible by adding an OSK

--- configuration/novnc.sh
+++ configuration/novnc.sh
@@ -8,7 +8,7 @@ CONFIG_FILE="/etc/vncsecret@${POJDE_NG_USERNAME}"
 x11vnc -storepasswd ${POJDE_NG_PASSWORD} $CONFIG_FILE
 chown "${POJDE_NG_USERNAME}" $CONFIG_FILE
 
-# Enable & restart the services
+# These are the services that will need to be managed
 services=(
     xvfb
     desktop
@@ -16,7 +16,20 @@ services=(
     novnc
 )
 
+# Enable & restart the services
 if [ "${POJDE_NG_OPENRC}" = 'true' ]; then
+    # Generate initial config files by starting Fluxbox once without an X server
+    sudo -u ${POJDE_NG_USERNAME} startfluxbox || echo "Generated Fluxbox config files"
+
+    # Enable left mouse button click for menu on desktop
+    sed -i '1s/^/\OnDesktop Mouse1\ \:RootMenu\n/' /home/${POJDE_NG_USERNAME}/.fluxbox/keys
+
+    # Add On-Screen Keyboard menu option
+    echo '?package(bash):needs="text" section="Accessibility" title="On-Screen Keyboard" command="/usr/bin/matchbox-keyboard"' >/usr/share/menu/matchbox-keyboard
+
+    # Update menus
+    update-menus
+
     for service in "${services[@]}"; do
         rc-update add "${service}" default
         rc-service "${service}" restart
