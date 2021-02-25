19ad7d7374dda75081516c03fd97ee8dda250d69
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-02-26 00:12:33 +0100

    feat: Add Vim tool module and proper shortcut setup

--- configuration/code-server.sh
+++ configuration/code-server.sh
@@ -11,11 +11,184 @@ auth: password
 password: "${POJDE_NG_PASSWORD}"
 EOT
 
+# Create the config dir
+CONFIG_DIR=/home/${POJDE_NG_USERNAME}/.local/share/code-server/User/
+mkdir -p ${CONFIG_DIR}
+
+# Add web-optimized shortcuts
+cat <<EOT >${CONFIG_DIR}/keybindings.json
+[
+  {
+    "command": "-editor.action.marker.nextInFiles",
+    "keybinding": "f8",
+    "when": "editorFocus && !editorReadonly",
+    "resolved": [
+      {
+        "key": {
+          "code": "F8",
+          "keyCode": 119,
+          "easyString": "f8"
+        },
+        "ctrl": false,
+        "shift": false,
+        "alt": false,
+        "meta": false
+      }
+    ],
+    "scope": 1,
+    "key": "f8"
+  },
+  {
+    "command": "editor.action.marker.nextInFiles",
+    "keybinding": "alt+p",
+    "when": "editorFocus && !editorReadonly",
+    "resolved": [
+      {
+        "key": {
+          "code": "KeyP",
+          "keyCode": 80,
+          "easyString": "p"
+        },
+        "ctrl": false,
+        "shift": false,
+        "alt": true,
+        "meta": false
+      }
+    ],
+    "scope": 1,
+    "key": "alt+p"
+  },
+  {
+    "command": "-file.rename",
+    "keybinding": "f2",
+    "context": "navigatorActive",
+    "resolved": [
+      {
+        "key": {
+          "code": "F2",
+          "keyCode": 113,
+          "easyString": "f2"
+        },
+        "ctrl": false,
+        "shift": false,
+        "alt": false,
+        "meta": false
+      }
+    ],
+    "scope": 1,
+    "key": "f2"
+  },
+  {
+    "command": "file.rename",
+    "keybinding": "alt+r",
+    "context": "navigatorActive",
+    "resolved": [
+      {
+        "key": {
+          "code": "KeyR",
+          "keyCode": 82,
+          "easyString": "r"
+        },
+        "ctrl": false,
+        "shift": false,
+        "alt": true,
+        "meta": false
+      }
+    ],
+    "scope": 1,
+    "key": "alt+r"
+  },
+  {
+    "command": "editor.action.rename",
+    "keybinding": "alt+r",
+    "when": "editorHasRenameProvider && editorTextFocus && !editorReadonly",
+    "resolved": [
+      {
+        "key": {
+          "code": "KeyR",
+          "keyCode": 82,
+          "easyString": "r"
+        },
+        "ctrl": false,
+        "shift": false,
+        "alt": true,
+        "meta": false
+      }
+    ],
+    "scope": 1,
+    "key": "alt+r"
+  },
+  {
+    "command": "-editor.action.rename",
+    "keybinding": "f2",
+    "when": "editorHasRenameProvider && editorTextFocus && !editorReadonly",
+    "resolved": [
+      {
+        "key": {
+          "code": "F2",
+          "keyCode": 113,
+          "easyString": "f2"
+        },
+        "ctrl": false,
+        "shift": false,
+        "alt": false,
+        "meta": false
+      }
+    ],
+    "scope": 1,
+    "key": "f2"
+  },
+  {
+    "command": "editor.action.goToReferences",
+    "keybinding": "alt+i",
+    "when": "editorHasReferenceProvider && editorTextFocus && !inReferenceSearchEditor && !isInEmbeddedEditor",
+    "key": "alt+i"
+  },
+  {
+    "command": "-editor.action.goToReferences",
+    "keybinding": "shift+f12",
+    "when": "editorHasReferenceProvider && editorTextFocus && !inReferenceSearchEditor && !isInEmbeddedEditor",
+    "key": "shift+f12"
+  },
+  {
+    "key": "alt+w",
+    "command": "workbench.action.closeActiveEditor"
+  },
+  {
+    "key": "shift+alt+p",
+    "command": "workbench.action.showCommands"
+  },
+  {
+    "key": "f1",
+    "command": "-workbench.action.showCommands"
+  },
+  {
+    "key": "ctrl+shift+\`",
+    "command": "workbench.action.terminal.new"
+  },
+  {
+    "key": "ctrl+shift+c",
+    "command": "-workbench.action.terminal.new"
+  }
+]
+EOT
+
+# Add web-optimized configuration
+cat <<EOT >${CONFIG_DIR}/settings.json
+{
+  "editor.autoSave": "on",
+  "keyboard.dispatch": "keyCode"
+}
+EOT
+
+# Fix permissions for user
+chown -R ${POJDE_NG_USERNAME} /home/${POJDE_NG_USERNAME}/.local/share/code-server/
+
 # Enable & restart the services
 if [ "${POJDE_NG_OPENRC}" = 'true' ]; then
-    rc-service code-server restart
-    rc-update add code-server default
+  rc-service code-server restart
+  rc-update add code-server default
 else
-    systemctl enable "code-server@${POJDE_NG_USERNAME}"
-    systemctl restart "code-server@${POJDE_NG_USERNAME}"
+  systemctl enable "code-server@${POJDE_NG_USERNAME}"
+  systemctl restart "code-server@${POJDE_NG_USERNAME}"
 fi
---
+++ modules/tool.vim.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Root script
+function as_root() {
+    :
+}
+
+# User script
+function as_user() {
+    # We'll use Open-VSX
+    export SERVICE_URL=https://open-vsx.org/vscode/gallery
+    export ITEM_URL=https://open-vsx.org/vscode/item
+
+    # Install the VSCodeVim VSCode extension
+    code-server --install-extension 'vscodevim.vim'
+}
