9bdf3de0a0abf552d484e0d5088d32b315d9283d
Felix Pojtinger <felix@pojtinger.com>
2021-02-26 00:12:33 +0100

    feat: Add Vim tool module and proper shortcut setup

--- configuration/git.sh
+++ configuration/git.sh
@@ -3,7 +3,8 @@
 # Read configuration file
 . /opt/pojde-ng/preferences/preferences.sh
 
-# Set up Git pull strategy, email and password
+# Set up Git pull strategy, branch, email and password
 su - $POJDE_NG_USERNAME -c "git config --global pull.rebase false"
+su - $POJDE_NG_USERNAME -c "git config --global init.defaultBranch main"
 su - $POJDE_NG_USERNAME -c "git config --global user.email \"${POJDE_NG_EMAIL}\""
 su - $POJDE_NG_USERNAME -c "git config --global user.name \"${POJDE_NG_FULL_NAME}\""
--- configuration/parameters.sh
+++ configuration/parameters.sh
@@ -41,6 +41,7 @@ available_modules=(
     lang.python Python $(${POJDE_NG_MODULE_PYTHON_ENABLED} && echo on || echo off)
     lang.rust Rust $(${POJDE_NG_MODULE_RUST_ENABLED} && echo on || echo off)
     lang.javascript JavaScript $(${POJDE_NG_MODULE_JAVASCRIPT_ENABLED} && echo on || echo off)
+    tool.vim Vim $(${POJDE_NG_MODULE_VIM_ENABLED} && echo on || echo off)
 )
 selected_modules="$(dialog --stdout --nocancel --checklist "Additional modules to install:" 0 0 0 ${available_modules[@]})"
 
@@ -49,6 +50,7 @@ echo export "'"POJDE_NG_MODULE_GO_ENABLED=$([[ "$selected_modules" == *"lang.go"
 echo export "'"POJDE_NG_MODULE_PYTHON_ENABLED=$([[ "$selected_modules" == *"lang.python"* ]] && echo true || echo false)"'" >>${TMP_PREFERENCE_FILE}
 echo export "'"POJDE_NG_MODULE_RUST_ENABLED=$([[ "$selected_modules" == *"lang.rust"* ]] && echo true || echo false)"'" >>${TMP_PREFERENCE_FILE}
 echo export "'"POJDE_NG_MODULE_JAVASCRIPT_ENABLED=$([[ "$selected_modules" == *"lang.javascript"* ]] && echo true || echo false)"'" >>${TMP_PREFERENCE_FILE}
+echo export "'"POJDE_NG_MODULE_VIM_ENABLED=$([[ "$selected_modules" == *"tool.vim"* ]] && echo true || echo false)"'" >>${TMP_PREFERENCE_FILE}
 
 # Persist checklist selection
 echo export "'"POJDE_NG_MODULES=${selected_modules}"'" >>${TMP_PREFERENCE_FILE}
