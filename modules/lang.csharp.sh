0453da935ce2f3024a025a169d960e7d309863e9
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-02-26 01:14:28 +0100

    feat: Make C# language pack usable without restarting Jupyter

--- modules/lang.csharp.sh
+++ modules/lang.csharp.sh
@@ -8,22 +8,23 @@ function as_root() {
     # Fetch and install Microsoft keys package
     curl -L -o /tmp/packages-microsoft-prod.deb https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb
     dpkg -i /tmp/packages-microsoft-prod.deb
+    rm /tmp/packages-microsoft-prod.deb
 
-    # Install .NET
+    # Install C#
     apt update
     apt install -y dotnet-sdk-5.0 aspnetcore-runtime-5.0
 
-    # Remove the Microsoft keys package
-    rm /tmp/packages-microsoft-prod.deb
-
-    # Add .NET tools to PATH using profile
-    CONFIG_FILE=/etc/profile.d/dotnet.sh
+    # Add C# tools to PATH using profile
+    CONFIG_FILE=/etc/profile.d/csharp.sh
     echo "export PATH=\$PATH:/home/${POJDE_NG_USERNAME}/.dotnet/tools" >${CONFIG_FILE}
     chmod +x ${CONFIG_FILE}
 
-    # Add .NET tools to both .bashrcs
+    # Add C# tools to both .bashrcs
     echo ". ${CONFIG_FILE}" >>/root/.bashrc
     echo ". ${CONFIG_FILE}" >>/home/${POJDE_NG_USERNAME}/.bashrc
+
+    # Restart Jupyter Lab (so that the new PATH is re-read)
+    systemctl restart jupyter-lab@${POJDE_NG_USERNAME}
 }
 
 # User script
@@ -38,7 +39,7 @@ function as_user() {
     # Install the C# VSCode extension
     code-server --install-extension 'muhammad-sammy.csharp'
 
-    # Download the .NET Jupyter Kernel (see https://github.com/dotnet/interactive/blob/main/docs/NotebooksLocalExperience.md#installing-net-interactive-as-a-jupyter-kernel)
+    # Download the C# Jupyter Kernel (see https://github.com/dotnet/interactive/blob/main/docs/NotebooksLocalExperience.md#installing-net-interactive-as-a-jupyter-kernel)
     dotnet tool install -g --add-source "https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-tools/nuget/v3/index.json" Microsoft.dotnet-interactive
     dotnet interactive jupyter install
 }
