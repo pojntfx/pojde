91338c1735a6a0bf2936fce29539bcdd375c04e6
Felix Pojtinger <felix@pojtinger.com>
2021-03-03 20:00:54 +0000

    feat: Add APT caching

---
+++ configuration/apt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Upgrade script
+function upgrade() {
+    # Cache packages
+    CONFIG_FILE=/etc/apt/apt.conf.d/01-custom-local
+    cat <<EOT >$CONFIG_FILE
+Binary::apt::APT::Keep-Downloaded-Packages "true";
+EOT
+
+    # Remove the Docker overrides
+    rm -f /etc/apt/apt.conf.d/{docker-autoremove-suggests,docker-clean,docker-gzip-indexes,docker-no-languages}
+}
+
+# Refresh script
+function refresh() {
+    # Remove cached packages
+    rm /var/cache/apt/archives/*.deb
+}
