1f431b7696f7c03bff7145c2a9df7ab3e522ab08
Felix Pojtinger <felix@pojtinger.com>
2021-03-04 01:22:58 +0000

    refactor: Use 0-5 port range instead of 0-4,22

--- build/ssh.sh
+++ build/ssh.sh
@@ -5,8 +5,8 @@ if [ "${POJDE_NG_OPENRC}" = 'true' ]; then
     # Install dropbear (OpenSSH server requires systemd)
     apt install -y dropbear
 
-    # Change listen port to 38022 & only listen on loopback
-    sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT="127.0.0.1:38022"/g' /etc/default/dropbear
+    # Change listen port to 38005 & only listen on loopback
+    sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT="127.0.0.1:38005"/g' /etc/default/dropbear
 else
     # Install OpenSSH server
     apt install -y openssh-server
@@ -14,8 +14,8 @@ else
     # Enable TCP forwarding
     sed -i 's/#AllowTcpForwarding no/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
 
-    # Change listen port to 38022
-    sed -i 's/#Port 22/Port 38022/g' /etc/ssh/sshd_config
+    # Change listen port to 38005
+    sed -i 's/#Port 22/Port 38005/g' /etc/ssh/sshd_config
 
     # Only listen on loopback
     sed -i 's/#ListenAddress 0.0.0.0/ListenAddress 127.0.0.1/g' /etc/ssh/sshd_config
