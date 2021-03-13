405cc54560737470f8a3613e2b254586f51699cc
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-03-13 20:51:07 +0000

    feat: Enable mounting volumes from dind with docker-pojde-volume-setup

--- configuration/docker.sh
+++ configuration/docker.sh
@@ -15,5 +15,21 @@ function upgrade() {
 
 # Refresh script
 function refresh() {
+    # Read configuration file
+    . /opt/pojde/preferences/preferences.sh
+
+    # Add docker-pojde-volume-setup command, which enables using volumes
+    CONFIG_FILE=/etc/profile.d/docker.sh
+    cat <<EOT >$CONFIG_FILE
+function docker-pojde-volume-setup() {
+    container_name=\$(docker inspect -f "{{.Name}}" $(hostname))
+    export PWD="\$(docker volume inspect -f {{.Mountpoint}} \${container_name##/}-home-user)\${PWD##/home}"
+}
+EOT
+    chmod +x ${CONFIG_FILE}
+
+    # Add docker-pojde-volume-setup command to both .bashrcs
+    echo ". ${CONFIG_FILE}" >>/root/.bashrc
+    echo ". ${CONFIG_FILE}" >>/home/${POJDE_USERNAME}/.bashrc
     :
 }
