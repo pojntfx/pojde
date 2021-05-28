364013f667fcb8499c1644e4f2018a49c2650d0a
Felix Pojtinger <felix@pojtinger.com>
2021-05-28 11:11:11 +0200

    fix: Use new WebWormhole architecture identifiers (#3)

--- build/webwormhole.sh
+++ build/webwormhole.sh
@@ -2,9 +2,5 @@
 
 # Download the binary & add it to the PATH
 VERSION="latest"
-if [ "$(uname -m)" = 'x86_64' ]; then
-    curl -L -o /usr/bin/ww https://github.com/pojntfx/webwormhole-binaries/releases/download/${VERSION}/ww.linux-amd64
-else
-    curl -L -o /usr/bin/ww https://github.com/pojntfx/webwormhole-binaries/releases/download/${VERSION}/ww.linux-arm64
-fi
+curl -L -o /usr/bin/ww https://github.com/pojntfx/webwormhole-binaries/releases/download/${VERSION}/ww.linux-$(uname -m)
 chmod +x /usr/bin/ww
