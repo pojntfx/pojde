6829fc9cc768bcddc66ea6f6117cb4c71c4f80fb
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-06-08 19:18:41 +0200

    feat: Add RVM ot Ruby module

--- modules/lang.ruby.sh
+++ modules/lang.ruby.sh
@@ -3,7 +3,19 @@
 # Root script
 function as_root() {
     # Install Ruby and dependencies
-    apt install -y ruby-full libtool libffi-dev ruby-dev make libzmq3-dev libczmq-dev
+    apt install -y curl make git build-essential gnupg2 procps libtool libffi-dev make libzmq3-dev libczmq-dev libgdbm-dev libncurses-dev libssl-dev libreadline-dev libyaml-dev zlib1g-dev bison
+
+    # Install and activate the Ruby Version Manager
+    gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    curl -sSL https://get.rvm.io | bash -s stable
+    source /etc/profile.d/rvm.sh
+
+    # Install Ruby version
+    rvm install 2.7.1
+    rvm use 2.7.1
+
+    # Install Bundler
+    gem install bundler --no-ri
 
     # Install the Ruby debugger
     gem install rake
