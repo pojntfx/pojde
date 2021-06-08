7790e743e256660712eb64a1bdd903697cd2e488
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-06-08 14:17:37 +0200

    feat: Add Python's bin directory to PATH

--- modules/lang.python.sh
+++ modules/lang.python.sh
@@ -2,8 +2,23 @@
 
 # Root script
 function as_root() {
+    # Read configuration file
+    . /opt/pojde/preferences/preferences.sh
+
     # Set pip3 as default pip (Python 2 is EOL)
     update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
+
+    # Add Python's bin directory to PATH using profile
+    CONFIG_FILE=/etc/profile.d/python.sh
+    cat <<EOT >$CONFIG_FILE
+export PATH=\$PATH:/root/.local/bin
+export PATH=\$PATH:/home/${POJDE_USERNAME}/.local/bin
+EOT
+    chmod +x ${CONFIG_FILE}
+
+    # Add Python's bin directory to both .bashrcs
+    echo ". ${CONFIG_FILE}" >>/root/.bashrc
+    echo ". ${CONFIG_FILE}" >>/home/${POJDE_USERNAME}/.bashrc
 }
 
 # User script
